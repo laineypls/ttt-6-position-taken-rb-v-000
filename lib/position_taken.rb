e8c7bf66cdf85279ebe97664f9335ed587331314
Elaine C <laineypls@gmail.com>
2019-05-31 08:31:11 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,7 +3,7 @@
 def position_taken?(board, index)
   if board[index] = ""
     return false
-  elsif board[index] = "X" || board[index] "O"
+  else board[index] = "X" || board[index] "O"
     return true
   end
 end
